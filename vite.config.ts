9fc7c5d8751bc5caa9cf5487834c77b24d2dcae2
Mohamed <mohamedikbal32@gmail.com>
2025-03-21 04:22:32 +0100

    Update deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/edulearn/',
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
-});
+}));
